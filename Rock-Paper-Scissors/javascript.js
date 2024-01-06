dfe7cb0171f4710270fcee2a9c6ba35d2d4473b7
Roshan <roshanmichael.odin.com>
2024-01-06 10:48:45 -0600

    The project is completed

--- Rock-Paper-Scissors/javascript.js
+++ Rock-Paper-Scissors/javascript.js
@@ -23,16 +23,51 @@ function singleRound(playerSelection, computerSelection)
         (playerSelection == "paper" && computerSelection == "scissors") ||
         (playerSelection == "scissors" && computerSelection == "rock"))
     {
+        
         return "You lose!";
     }
 
     else if(playerSelection == computerSelection)
     {
+        
         return "TIE";
     }
 
     else
     {
+        
         return "You won!";
+    }
+}
+
+function game(a)
+{
+    let playerCounter = 0, ComputerCounter = 0;
+
+    for(let i = 0; i < 5; i++)
+    {
+        let player = singleRound(a, getComputerChoice());
+        
+        if(player == "You won!")
+        {
+            playerCounter++;
+        }
+        else if(player == "TIE")
+        {
+            playerCounter = playerCounter + 0;
+        }
+        else
+        {
+            ComputerCounter++;
+        }
+    }
+
+    if (playerCounter > ComputerCounter)
+    {
+        console.log("player won by " + playerCounter);
+    }
+    else
+    {
+        console.log("Computer won by " + ComputerCounter);
     }
 }
